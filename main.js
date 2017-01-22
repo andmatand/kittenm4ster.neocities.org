c2b563416a7e68634199930611d8b56de443da50
Andrew Anderson <andrewloveskitties@gmail.com>
2017-01-22 12:15:28 -0800

    Refactor color-changing js: use CSS3 transitions

--- main.js
+++ main.js
@@ -9,9 +9,10 @@
 
     var colorState = {
         srcIndex: null,
-        destIndex: null,
-        percent: 0
+        destIndex: null
     };
+
+    var target = document.body;
 
     function random_int(min, max) {
         min = Math.ceil(min);
@@ -19,62 +20,56 @@
         return Math.floor(Math.random() * (max - min)) + min;
     }
 
-    function shift_color() {
-        if (colorState.destIndex === null) {
-            // Choose which direction to go around the palette circle
-            var delta;
-            if (random_int(0, 1) === 0) {
-                delta = -1;
-            } else {
-                delta = 1;
-            }
+    function choose_next_color() {
+        colorState.srcIndex = colorState.destIndex;
 
-            // Get the index of the color in the direction we chose
-            colorState.destIndex = colorState.srcIndex + delta;
-
-            // Wrap around the palette circle
-            if (colorState.destIndex < 0) {
-                colorState.destIndex = DISCO_PALETTE.length - 1;
-            } else if (colorState.destIndex > DISCO_PALETTE.length - 1) {
-                colorState.destIndex = 0;
-            }
-
-            // Start at 0% fade
-            colorState.percent = 0;
+        // Choose which direction to go around the palette circle
+        var delta;
+        if (random_int(0, 1) === 0) {
+            delta = -1;
+        } else {
+            delta = 1;
         }
 
-        // Get the current RGB values based on the fade percent
+        // Get the index of the color in the direction we chose
+        colorState.destIndex = colorState.srcIndex + delta;
+
+        // Wrap around the palette circle
+        if (colorState.destIndex < 0) {
+            colorState.destIndex = DISCO_PALETTE.length - 1;
+        } else if (colorState.destIndex > DISCO_PALETTE.length - 1) {
+            colorState.destIndex = 0;
+        }
+    }
+
+    function change_color() {
+        // Get the current RGB values based on destIndex
         var rgb = {};
         for (var i = 0; i < 3; i++) {
-            var src = DISCO_PALETTE[colorState.srcIndex][i];
-            var dest = DISCO_PALETTE[colorState.destIndex][i];
-            var percent = colorState.percent;
-
-            rgb[i] = Math.floor(src + (percent * (dest - src)));
+            rgb[i] = DISCO_PALETTE[colorState.destIndex][i];
         }
 
         // Set the background color
-        document.body.style.backgroundColor = 'rgb('
+        target.style.backgroundColor = 'rgb('
             + rgb[0] + ','
             + rgb[1] + ','
             + rgb[2] + ')';
+    }
 
-        // Increment the fade percent
-        var changeDelta = .005;
-        colorState.percent += changeDelta;
-
-        // If we have reached the destination color
-        if (colorState.percent >= 1) {
-            colorState.srcIndex = colorState.destIndex;
-            colorState.destIndex = null;
-        }
+    function on_fade_finished() {
+        choose_next_color();
+        change_color();
     }
 
     function init() {
         // Choose a random color to start
         colorState.srcIndex = random_int(0, DISCO_PALETTE.length - 1);
 
-        window.setInterval(shift_color, 33);
+        // Listen for the end of the color-change transition
+        target.addEventListener('transitionend', on_fade_finished, false);
+
+        // Trigger the first color-change
+        window.setTimeout(on_fade_finished, 100);
     }
 
     // Run
